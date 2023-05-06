828be41f7c53f7ad1233c78ad942323d23f5df63
Luis S <Luissalcedo2355@hotmail.com>
2023-05-06 14:15:41 -0700

    intro done

---
+++ intro.js
@@ -0,0 +1,68 @@
+//variables
+const randomCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let interval = 0; 
+
+//query Selectors
+const introText = document.querySelector("#intro-text");
+const inputLock =document.querySelector("#inpLock")
+const calimReward = document.querySelector("#claim-reward");
+const introContainer = document.querySelector("#intro-container");
+
+//lockhover & noHover
+const DaLock = document.querySelector(".locks");
+const HoverText = document.querySelector("#hover-text");
+
+//audio
+let closeIntroAudio = new Audio('./resources/unlock_click.wav');
+
+//functions
+function runIntro() {
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    introText.innerText = introText.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return introText.dataset.value[index];
+        }
+      
+        return randomCharacters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= introText.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 4;
+  }, 25);
+
+  setTimeout(()=>{
+      inputLock.checked = false;
+      calimReward.addEventListener("click", ()=>closeIntro())
+  }, 1850)
+}
+
+      
+    
+
+
+function lockHover () {
+  DaLock.style.display= "none";
+  HoverText.style.display= "inline";
+}
+function lockNoHover () {
+  DaLock.style.display= "grid";
+  HoverText.style.display= "none";
+}
+
+function closeIntro () {
+  closeIntroAudio.play();
+  introContainer.style.display= "none"
+}
+
+
+
